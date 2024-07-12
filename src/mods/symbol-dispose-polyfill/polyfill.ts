b86100ce3d87eb80f7e6096b6bc38dd1d8845c57
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-07-12 23:23:21 +0700

    clean

--- src/mods/symbol-dispose-polyfill/polyfill.ts
+++ src/mods/symbol-dispose-polyfill/polyfill.ts
@@ -1,15 +1,5 @@
 if (typeof Symbol.dispose !== "symbol")
-  Object.defineProperty(Symbol, "dispose", {
-    configurable: false,
-    enumerable: false,
-    writable: false,
-    value: Symbol.for("dispose")
-  })
+  Object.defineProperty(Symbol, "dispose", { value: Symbol.for("dispose") })
 
 if (typeof Symbol.asyncDispose !== "symbol")
-  Object.defineProperty(Symbol, "asyncDispose", {
-    configurable: false,
-    enumerable: false,
-    writable: false,
-    value: Symbol.for("asyncDispose")
-  })
+  Object.defineProperty(Symbol, "asyncDispose", { value: Symbol.for("asyncDispose") })
